9d5714add365ce09f30c61c6afc7f6e3012132da
Mauro de Lyon <maurodelyon@gmail.com>
2015-12-01 12:43:54 +0100

    remasterd

--- HueLamp/HueLamp/View/LampSettingsPage.xaml.cs
+++ HueLamp/HueLamp/View/LampSettingsPage.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace HueLamp.View
+namespace HueLamp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class LampSettings : Page
+    public sealed partial class LampSettingsPage : Page
     {
-        public LampSettings()
+        public LampSettingsPage()
         {
             this.InitializeComponent();
         }
